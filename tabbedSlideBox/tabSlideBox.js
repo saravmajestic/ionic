54e64ce5d6f28a2b22fe64f23b604c45a969e04f
sarshanmugam <sarshanmugam@ebay.com>
2014-07-27 23:22:50 -0700

    Adding tabbed Slide box component

---
+++ tabbedSlideBox/tabSlideBox.js
@@ -0,0 +1,75 @@
+/*
+ * SimplePubSub from https://github.com/mbenford/ngTagsInput/blob/master/src/util.js
+ * */
+'use strict';
+
+function SimplePubSub() {
+    var events = {};
+    return {
+        on: function(names, handler) {
+            names.split(' ').forEach(function(name) {
+                if (!events[name]) {
+                    events[name] = [];
+                }
+                events[name].push(handler);
+            });
+            return this;
+        },
+        trigger: function(name, args) {
+            angular.forEach(events[name], function(handler) {
+                handler.call(null, args);
+            });
+            return this;
+        }
+    };
+};
+
+angular.module('tabSlideBox', [])
+.directive('tabSlideBox', [ '$timeout', '$window', '$ionicSlideBoxDelegate',
+	function($timeout, $window, $ionicSlideBoxDelegate) {
+		'use strict';
+
+		return {
+			restrict : 'A, E, C',
+			link : function(scope, element, attrs, ngModel) {
+				
+				var ta = element[0], $ta = element;
+				$ta.addClass("tabbed-slidebox");
+				
+				var iconsDiv = angular.element(ta.querySelector(".tsb-icons")), icons = iconsDiv.find("a");
+				angular.forEach(icons, function(value, key){
+				     var a = angular.element(value);
+				     a.on('click', function(){
+				    	 $ionicSlideBoxDelegate.slide(key);
+				     });
+				});
+				
+				function setPosition(index){
+					var middle = iconsDiv[0].offsetWidth/2, wrap = iconsDiv[0].querySelector(".tsb-ic-wrp");
+					wrap.style.left = "0px";
+					var initLeft = wrap.offsetLeft;
+					var leftStr = (middle - initLeft -  (42*(index + 1)) +  22) + "px";
+					wrap.style.webkitTransform =  "translate3d("+leftStr+",0,0)" ;
+					
+					angular.element(iconsDiv[0].querySelector(".active")).removeClass("active");
+					angular.element(icons[index]).addClass("active");
+				};
+				
+				var events = scope.events;
+				events.on('slideChange', function(data){
+					setPosition(data.index);
+				});
+				
+				setPosition(Math.floor(icons.length/2));
+			},
+			controller : function($scope, $attrs, $element) {
+				$scope.events = new SimplePubSub();
+				
+				$scope.slideHasChanged = function(index){
+					$scope.events.trigger("slideChange", {"index" : index});
+				};
+			}
+		};
+
+	} 
+]);
